a93c6933f030203d33005949e2314c9a5f793af7
elkinssm <elkinsilvamanrique@gmail.com>
2023-05-05 17:35:49 -0500

    add comments

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,17 +8,20 @@ Page({
       this.hideLoading();
     }, 3000); // Personalizar el tiempo de carga del loading
   },
+  //Metodo para mostrar desde algun boton o evento
   onTapButton() {
     this.showLoading();
     setTimeout(() => {
       this.hideLoading();
     }, 3000); // Personalizar el tiempo de carga del loading en el boton
   },
+  //Metodo necesario para mostrar el loading
   showLoading() {
     this.setData({
-      showLoading: true,
+      showLoading: true
     });
   },
+  //Metodo necesario para ocultar el loading
   hideLoading() {
     this.setData({
       showLoading: false
