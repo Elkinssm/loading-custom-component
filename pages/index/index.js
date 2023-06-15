c37c3a22369fce612741150a6f9b31612cca82e5
elkinssm <elkinsilvamanrique@gmail.com>
2023-06-15 09:34:38 -0500

    fix time

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,14 +6,14 @@ Page({
     this.showLoading();
     setTimeout(() => {
       this.hideLoading();
-    }, 30000); // Personalizar el tiempo de carga del loading
+    }, 3000); // Personalizar el tiempo de carga del loading
   },
   //Metodo para mostrar desde algun boton o evento
   onTapButton() {
     this.showLoading();
     setTimeout(() => {
       this.hideLoading();
-    }, 30000); // Personalizar el tiempo de carga del loading en el boton
+    }, 3000); // Personalizar el tiempo de carga del loading en el boton
   },
   //Metodo necesario para mostrar el loading
   showLoading() {
